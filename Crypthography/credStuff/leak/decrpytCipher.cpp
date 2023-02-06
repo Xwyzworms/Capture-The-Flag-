8f75a20d405b624abb7c0007c792b4e1f0018393
Xwyzworms <pratamaazmi1@gmail.com>
2023-02-06 18:53:15 -0500

    finalize the script, decrypt all passwords

--- Crypthography/credStuff/leak/decrpytCipher.cpp
+++ Crypthography/credStuff/leak/decrpytCipher.cpp
@@ -1,9 +1,43 @@
+/*
+    Creator : Xoros Lacknatas Ingradu Jernair
+    Description : What is Description ?
+    Qoute : Simply say its okay
+    DATE : Uknown
+*/
+
 #include<iostream>
 #include<string>
 #include<vector>
 #include<cctype>
 #include<fstream>
 
+std::string solve(std::string cipher) 
+{
+    std::string ans;
+    for(int i = 0 ; i<cipher.size(); i++) 
+    {
+
+        if(std::isalpha(cipher[i])) 
+        {
+            int theChar = static_cast<int>(cipher[i]) + 13;
+            char trueRepresentation ;
+            if(theChar > 122) 
+            {
+                trueRepresentation = char(theChar - 26);
+            }
+            else 
+            {
+                trueRepresentation = char(theChar );
+            }
+            ans += trueRepresentation;
+        }
+        else 
+        {
+        ans += cipher[i];
+        }
+    }
+    return ans;
+}
 
 std::vector<std::string> readFile(const std::string filename) 
 {
@@ -32,49 +66,27 @@ std::string toLowerStr(std::string str)
     }
     return res;
 }
-
-void solve(std::string cipher) 
-{
-    std::vector<char> ans;
-    for(int i = 0 ; i<cipher.size(); i++) 
-    {
-
-        if(std::isalpha(cipher[i])) 
+void writeFile(const std::vector<std::string> contents,
+               const std::vector<std::string> passwords) 
         {
-
-            int theChar = static_cast<int>(cipher[i]) + 13;
-            char trueRepresentation ;
-            if(theChar > 122) 
+            std::ofstream outputFile("solutions.txt");
+            std::string converted = "";
+            for(int i = 0 ; i < passwords.size() ; i++) 
             {
-                trueRepresentation = char(theChar - 26);
+                converted = solve(toLowerStr(passwords[i]));
+                std::string c = contents[i];
+                c.append(" : ").append(converted).append("\n");
+                outputFile << c;
             }
-            else 
-            {
-                trueRepresentation = char(theChar );
-            }
-            ans.push_back(trueRepresentation);
+            outputFile.close();
+            std::cout<<"File written\n";
         }
-        else 
-        {
-        ans.push_back(cipher[i]);
-        }
-    }
-
-    for(auto a : ans) 
-    {
-        std::cout<<a;
-    }
-}
-
-void display() 
-{
-
-}
 
 int main() 
 {
-    std::string passwd = toLowerStr("cvpbPGS{P7e1S_54I35_71Z3}");
-    std::cout<<passwd<<std::endl;
-    solve(passwd);
+    std::vector<std::string> contents = readFile("usernames.txt");
+    std::vector<std::string> passwords = readFile("passwords.txt");
+    writeFile(contents, passwords);
+    std::cout<<solve(toLowerStr("cvpbPGS{P7e1S_54I35_71Z3}"))<<std::endl;
     return 0;
 }
